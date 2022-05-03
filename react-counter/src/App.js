f3000ebfddcd509d82de1ce9b3d32f664f6e44a5
William Dias <smdwilliam89@gmail.com>
2022-05-03 21:07:44 +0100

    mais uma aula finalizada

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -1,11 +1,44 @@
+import React, { useState } from 'react';
 
-import React from 'react';
+import Albums from './Components/Albums';
 import Counter from './Components/Counter';
+import Users from './Components/Users';
+
+const defaultPage = 'albums';
+
+const pages = {
+  albums: {
+    text: 'Albums',
+    component: Albums,
+  },
+  counter: {
+    text: 'Counter',
+    component: Counter,
+  },
+  users: {
+    text: 'Users',
+    component: Users,
+  }
+}
 
 function App() {
+  const [page, setPage] = useState(defaultPage);
+
+  const handleChangePage = page => {
+    setPage(page);
+  }
+
+  const Page = pages[page].component;
+
+  const pageNames = Object.keys(pages);
+
   return (
     <>
-      <Counter />
+      {
+        pageNames.map(page => <button onClick={() => handleChangePage(page) }>{ pages[page].text }</button>)
+      }
+
+      { Page && <Page /> }
     </>
   )
 }
