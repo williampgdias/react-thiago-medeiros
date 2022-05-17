2b9cf15adb57aef3136380ed66e7684327e38b6f
William Dias <smdwilliam89@gmail.com>
2022-05-17 12:27:49 +0100

    Adicionando cadastro de clientes

--- my_app/src/pages/customers/Register.js
+++ my_app/src/pages/customers/Register.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { TextField, Button } from '@material-ui/core';
+
+import Toasty from '../../components/Toasty'
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
@@ -12,19 +15,103 @@ const useStyles = makeStyles((theme) => ({
 const Register = () => {
   const classes = useStyles();
 
+  const [form,setForm] = useState({
+    name: {
+      value: '',
+      error: false,
+    },
+    job: {
+      value: '',
+      error: false,
+    },
+  })
+
+  const [openToasty, setOpenToasty] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: {
+        value,
+      },
+    })
+  }
+
+  const handleRegisterButton = () => {
+    setIsLoading(true);
+
+    let hasError = false;
+    let newFormState = {
+      ...form,
+    }
+    if (!form.name.value) {
+      hasError = true;
+
+      newFormState.name = {
+        value: form.name.value,
+        error:true,
+        helperText: 'Incorrectly entry',
+      };
+    }
+    if (!form.job.value) {
+      hasError = true;
+
+      newFormState.job = {
+        value: form.job.value,
+        error:true,
+        helperText: 'Incorrectly entry',
+      };
+    }
+
+    if(hasError) {
+      return setForm(newFormState);
+    }
+
+    axios.post('https://reqres.in/api/users', {
+      name: form.name.value,
+      job: form.job.value,
+    }).then((response) => {
+      setOpenToasty(true);
+      setIsLoading(false);
+    })
+  }
+
   return (
     <>
       <div className={ classes.wrapper }>
-        <TextField label="Tell me your name" />
+        <TextField
+          error={ form.name.error }
+          helperText={ form.name.error ? form.name.helperText : '' }
+          label="Tell me your name" 
+          name="name" 
+          value={ form.name.value } 
+          onChange={ handleInputChange} />
       </div>
       <div className={ classes.wrapper }>
-        <TextField label="What do you do?" />
+        <TextField
+        error={ form.job.error }
+        helperText={ form.job.error ? form.job.helperText : '' } 
+        label="What do you do?"
+        name="job"
+        value={ form.job.value }
+        onChange={ handleInputChange} />
       </div>
       <div className={ classes.wrapper }>
-        <Button variant="contained" color="primary">
-          Sign In
+        <Button variant="contained" color="primary" onClick={ handleRegisterButton } disabled={ isLoading }>
+          {
+            isLoading ? 'Loading...' : "Sign In"
+          }
         </Button>
       </div>
+      <Toasty 
+        open={ openToasty }
+        severity="success"
+        text="Success"
+        onClose={ () => setOpenToasty(false) }
+      />
     </>
   )
 }
